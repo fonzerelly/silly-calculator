a458011aea865040061f93b27ab643ecebcfcc65
cs8433 <christian.hoerauf@consorsbank.de>
2019-11-12 00:25:26 +0100

    Implement first jsdom based test

--- scripts/calculator.test.js
+++ scripts/calculator.test.js
@@ -1,5 +1,23 @@
+const {JSDOM, VirtualConsole}  = require('jsdom')
+
 describe('calculator', () => {
-    it('should work', () => {
-        console.log('it works')
+    let document, mockLog
+    beforeEach( async () => {
+        // preparation of piping all loging inside the jsdom instance to a mock function
+        mockLog = jest.fn(() => {})
+        const virtualConsole = new VirtualConsole()
+        virtualConsole.on('log', mockLog)
+        
+        // instantiate window (asynchronously since it loads from a file)
+        const { window } = await JSDOM.fromFile('./index.html', { 
+            virtualConsole, 
+            runScripts: 'dangerously' // read https://github.com/jsdom/jsdom#executing-scripts
+        })
+        document = window.document
+    })
+    it('should recognize click in jsdom resulting in a console.log output', () => {
+        const btn = document.querySelector('button')
+        btn.click()
+        expect(spy.mock.calls[0][0]).toEqual('click')
     })
 })
