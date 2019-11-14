c22b1db9b31f0c5f4126eab5f745b3d77df6c41f
cs8433 <christian.hoerauf@consorsbank.de>
2019-11-14 10:45:36 +0100

    handles display properly

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -2,6 +2,9 @@ window.onload = () => {
     const display = document.getElementById('display')
     document.querySelectorAll('button').forEach((btn) => {
         btn.addEventListener('click', () => {
+            if (display.innerHTML === '0') {
+                display.innerHTML = ''
+            }
             display.innerHTML += btn.innerHTML
         })
     })
--- scripts/calculator.test.js
+++ scripts/calculator.test.js
@@ -19,4 +19,24 @@ describe('calculator', () => {
             expect(display.innerHTML).toMatch(new RegExp(numChar))
         })
     })
+
+    itWill('initial zero', () => {
+        const document = dom.window.document
+        const display = document.getElementById('display')
+        const button = document.querySelector('button')
+
+        expect(display.innerHTML).toEqual('0')
+        button.click()
+        expect(display.innerHTML).not.toEqual(button.innerHTML + button.innerHTML)
+    })
+
+    itWill('concatenate several number clicks in display', () => {
+        const document = dom.window.document
+        const display = document.getElementById('display')
+        const button = document.querySelector('button')
+
+        button.click()
+        button.click()
+        expect(display.innerHTML).toEqual(button.innerHTML + button.innerHTML)
+    })
 })
